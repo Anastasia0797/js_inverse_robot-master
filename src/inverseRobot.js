e3664fe82dd3d0cbfefedd6b33685e202256733e
Anastasia0797 <anasteyshavolevach@gmail.com>
2023-01-25 18:20:52 +0200

    homework

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,8 +6,33 @@
  * @return {object}
 */
 
+const kolli = {
+  Kolli: 'name',
+  123: 'chipVer',
+  3: 'wheels',
+};
+
+const robert = {
+  Robert: 'name',
+  123: 'chipVer',
+  113: 'chipVer',
+};
+
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+
+  for (const key in robot) {
+    if (result.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    result[robot[key]] = key;
+  }
+
+  return result;
 }
+
+inverseRobot(kolli);
+inverseRobot(robert);
 
 module.exports = inverseRobot;
